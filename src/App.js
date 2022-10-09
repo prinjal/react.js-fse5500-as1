075b7946cd80c119dfd86c08ef934b85267487ab
prinjal <prinjal.dave@gmail.com>
2022-10-09 19:30:59 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './styles.css';
+// import Navigation from "./components/navigation";
+// import Tuits from "./components/tuits";
+// import WhatsHappening from "./components/whats-happening/whats-happening";
+// import Bookmarks from "./components/bookmarks/bookmarks";
+import Tuiter from "./components/tuiter";
+
+function App() {
+  return (
+    <Tuiter/>
+  );
+}
+
+export default App;
