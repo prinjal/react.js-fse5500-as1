790abba28706ae25bec279a16c66f212b3e97e13
prinjal <prinjal.dave@gmail.com>
2022-11-17 03:36:52 -0500

    implemented like and changed icon

--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -1,28 +1,27 @@
-import React from "react";
-
-export default class TuitStats extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className="row mt-2">
-        <div className="col">
-          <i className="far fa-message me-1"></i>
-          {this.props.tuit.stats && this.props.tuit.stats.replies}
-        </div>
-        <div className="col">
-          <i className="far fa-retweet me-1"></i>
-          {this.props.tuit.stats && this.props.tuit.stats.retuits}
-        </div>
-        <div className="col">
-          <i className="far fa-heart me-1"></i>
-          {this.props.tuit.stats && this.props.tuit.stats.likes}
-        </div>
-        <div className="col">
-          <i className="far fa-inbox-out"></i>
-        </div>
+import React, {useEffect, useState} from "react";
+import * as service from "../../services/likes-service";
+const TuitStats = ({ tuit, likeTuit }) => {
+  return (
+    <div className="row mt-2">
+      <div className="col">
+        <i className="far fa-message me-1"></i>
+        {tuit.stats && tuit.stats.replies}
       </div>
-    );
-  }
+      <div className="col">
+        <i className="far fa-retweet me-1"></i>
+        {tuit.stats && tuit.stats.retuits}
+      </div>
+      <div className="col">
+        <span onClick={() => likeTuit(tuit)}>
+        <i class="fa-solid fa-thumbs-up"></i>
+          {tuit.stats && tuit.stats.likes}
+        </span>
+      </div>
+      <div className="col">
+        <i className="far fa-inbox-out"></i>
+      </div>
+    </div >
+  );
 }
+
+export default TuitStats;
